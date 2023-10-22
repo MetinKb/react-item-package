dce10eb42571bbe747286794dc30a904be35dea9
Metin Karabulut <114526516+MetinKb@users.noreply.github.com>
2023-10-22 20:33:16 +0300

    item statistics

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,8 +1,20 @@
-function Footer({ itemsLength }) {
+function Footer({ items }) {
+
+    const packedItems = items.filter(item => item.packed).length
+    const percentage = Math.round((packedItems / items.length) * 100)
+
     return (
-        <footer className="w-full h-[10%] bg-[#666] text-white f-center font-bold p-2">
-            <em>You have {itemsLength} items on your list. You already packed (X%)</em>
-        </footer>
+        < footer className="w-full h-[10%] bg-[#666] text-white f-center font-bold p-2" >
+            <em>{
+                items.length === 0 ? "Add some item to your pack! "
+                    :
+                    percentage === 100 ?
+                        'You packed everything! Ready to go ✈2'
+                        :
+                        `You have ${items.length} items on your list. You already packed ${packedItems}, (${percentage}%)`
+            }
+            </em>
+        </footer >
     )
 }
 
