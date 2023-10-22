4cb9aaa3288e98f2e94e7260c3a792e46baa8910
Metin Karabulut <114526516+MetinKb@users.noreply.github.com>
2023-10-22 20:34:54 +0300

    multidelete

--- src/Components/Items.js
+++ src/Components/Items.js
@@ -1,10 +1,11 @@
-function ItemPackage({ items, onDeleteItems, handleOnChangeItem }) {
+function ItemPackage({ items, onDeleteItems, handleOnChangeItem, multipleDelete }) {
 
     return (
-        <main className="bg-[#ffd35c] w-full h-[70%] p-4">
+        <main className="bg-[#f9c023] w-full h-[70%] p-4">
             {items.some(item => item.packed === true) ? (
-                <div className="f-center">
-                    <button className="p-2 rounded-lg bg-[#4db9e7] shadow-[0_0px_20px_3px_rgba(0,0,0,.3)] hover:brightness-90">Multiple Delete</button>
+                <div className="f-center mb-8 ">
+                    <button onClick={() => multipleDelete(items.filter(item => item.packed).map(item => item.id))}
+                        className="p-2 rounded-lg bg-[#777] text-white shadow-[0_0px_20px_3px_rgba(0,0,0,.5)] hover:brightness-90">Multiple Delete</button>
                 </div>
             ) : null}
             <ul className="flex flex-row items-start justify-end flex-wrap gap-7">
@@ -16,13 +17,19 @@ function ItemPackage({ items, onDeleteItems, handleOnChangeItem }) {
 
 function Item({ item, onDeleteItems, handleOnChangeItem }) {
     return (
-        <li className={`bg-orange-300 rounded-lg p-2 ${item.packed ? "brightness-75" : ""}`} >
-            <input type="checkbox" onChange={() => handleOnChangeItem(item.id)} value={item.packed} className="mr-2" />
-            < span style={item.packed ? { textDecoration: "line-through" } : {}} >
+        <li className={`bg-orange-200 rounded-lg p-2 shadow-[0_0px_20px_3px_rgba(0,0,0,.3)] ${item.packed ? "brightness-75" : ""}`}>
+            <input
+                type="checkbox"
+                checked={item.packed}
+                onChange={() => handleOnChangeItem(item.id)}
+                className="mr-2"
+            />
+            <span style={item.packed ? { textDecoration: "line-through" } : {}}>
                 {item.quantity} {item.description}
-            </span >
-            <button onClick={() => onDeleteItems(item.id)} className="ml-4 delete" >❌</button>
-        </ li >
+            </span>
+            <button onClick={() => onDeleteItems(item.id)} className="ml-4 delete">❌</button>
+        </li>
+
     )
 }
 
