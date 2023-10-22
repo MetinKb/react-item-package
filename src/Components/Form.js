656fb1ff023b0fdf9222dd5557d812814161f980
Metin Karabulut <114526516+MetinKb@users.noreply.github.com>
2023-10-22 20:36:26 +0300

    Update Form.js

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -21,8 +21,8 @@ function Form({ onAddItems }) {
 
     return (
         <form onSubmit={handlingSubmit} className="flex items-center gap-3 w-full h-[10%] f-center bg-[#d2e8ff]">
-            <h3 className="text-lg">What do you need for your trip 👜?</h3>
-            <select value={quantity} onChange={e => setQuantity(Number(e.target.value))} className="rounded-lg p-1 outline-none">
+            <h3 className="text-xl">What do you need for your trip 👜?</h3>
+            <select value={quantity} onChange={e => setQuantity(Number(e.target.value))} className="shadow-[0_0px_20px_3px_rgba(0,0,0,.3)] rounded-lg p-2 outline-none">
                 {
                     Array.from({ length: 20 }, (_, i) => i + 1).map((num, index) => (
                         <option value={num} key={index}>{num}</option>
@@ -30,9 +30,9 @@ function Form({ onAddItems }) {
 
                 }
             </select>
-            <input onChange={e => setDescription(e.target.value)} value={description} className="rounded-md p-1 outline-none"
+            <input onChange={e => setDescription(e.target.value)} value={description} className="shadow-[0_0px_20px_3px_rgba(0,0,0,.3)] rounded-md p-2 outline-none"
                 type="text" placeholder="Item Name..." />
-            <button className="bg-[#777] p-1 text-white rounded-lg hover:brightness-110">Add Item</button>
+            <button className="bg-[#777] p-2 text-white rounded-lg hover:brightness-110 shadow-[0_0px_20px_3px_rgba(0,0,0,.3)]">Add Item</button>
         </form>
     )
 }
