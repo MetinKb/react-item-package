8e2f40709a724799ec55379af86ad5446a68e76c
Metin Karabulut <114526516+MetinKb@users.noreply.github.com>
2023-10-22 20:33:58 +0300

    Delete Header.js

--- Header.js
+++
@@ -1,9 +0,0 @@
-function Header() {
-    return (
-        <header className="w-full h-[10%] bg-[#009fe3] f-center">
-            <h1 className="text-4xl font-Montserrat">🧳Travel Package ⛱️</h1>
-        </header>
-    )
-}
-
-export default Header
